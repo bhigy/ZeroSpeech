c7fe0c617b20d17e0a0e396ece7f3711d2205d42
Bertrand Higy <bertrand.higy@gmail.com>
2020-10-16 15:31:57 +0200

    using vq-analyze-code repo

--- preprocess.sh
+++ preprocess.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 python preprocess_val.py in_dir=/home/bjrhigy/corpora/flickr8k dataset=flickr8k/english
-python preprocess_val.py in_dir=/home/bjrhigy/dev/platalea/data/flickr8k_abx_wav/ dataset=flickr8k/english_triplets
+python preprocess_val.py in_dir=/home/bjrhigy/dev/vq-analyze-code/data/flickr8k_abx_wav/ dataset=flickr8k/english_triplets
